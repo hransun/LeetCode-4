2250622177041e3e62f0a034c0f1e6803fb8cd68
Lian <liulian1004@hotmail.com>
2020-07-21 12:05:36 -0700

    update

---
+++ 430-flatten-a-multilevel-doubly-linked-list/flattenamultileveldoublylinkedlist.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        //case1: cur.child != null && cur.next != null
+        //stack.offer(cur.next) ==> 因为cur.next要指向child了
+        //case2: cur.next == null && !stack.isEmpty()
+        // cur.next = stack.poll() == > cur走到头了，要和上一次的next链接在一起
+        //T: O(n) S:O(n)
+        if(head == null) {
+            return head;
+        }
+        Node cur = head;
+        Deque<Node> stack = new ArrayDeque<>();
+        while(cur != null) {
+            if(cur.child != null) {
+                // if cur has next
+                if(cur.next != null){
+                    stack.offerFirst(cur.next);
+                }
+                cur.next = cur.child;
+                cur.child.prev = cur;
+                cur.child = null;
+            }else if(cur.next == null && !stack.isEmpty()) {
+                cur.next = stack.pollFirst();
+                cur.next.prev = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
